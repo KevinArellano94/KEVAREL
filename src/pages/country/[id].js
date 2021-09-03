b3ee9dc44205e27cf848151e2d8de9c26b60fcfb
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-09-03 10:51:34 -0400

    Update [id].js

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,7 +1,35 @@
-const Country = () => {
+import Layout from "../../components/Layout/Layout";
+import styles from "./country.module.css";
+
+const Country = ({country}) => {
+    console.log(country);
     return (
-        <div>Country</div>
+        <Layout title={country.name}>
+            <div>
+                <div className={styles.overview}>
+                    <img src={country.flag} alt={country.name}></img>
+
+                    <h1>{country.name}</h1>
+                    <div>{country.region}</div>
+
+                    <div>
+                        <div className={styles.overview_population}></div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
 export default Country;
+
+export const getServerSideProps = async ({params}) => {
+
+    const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${params.id}`)
+
+    return {
+        props: {
+            country,
+        },
+    }
+}
