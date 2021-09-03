a2eba8de6271caa0dd3332fa120c58cf31fd4085
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-09-03 11:00:10 -0400

    Update [id].js

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,35 +1,35 @@
 import Layout from "../../components/Layout/Layout";
 import styles from "./country.module.css";
 
-const Country = ({country}) => {
-    console.log(country);
-    return (
-        <Layout title={country.name}>
-            <div>
-                <div className={styles.overview}>
-                    <img src={country.flag} alt={country.name}></img>
+// const Country = ({country}) => {
+//     console.log(country);
+//     return (
+//         <Layout title={country.name}>
+//             <div>
+//                 <div className={styles.overview}>
+//                     <img src={country.flag} alt={country.name}></img>
 
-                    <h1>{country.name}</h1>
-                    <div>{country.region}</div>
+//                     <h1>{country.name}</h1>
+//                     <div>{country.region}</div>
 
-                    <div>
-                        <div className={styles.overview_population}></div>
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    );
-}
+//                     <div>
+//                         <div className={styles.overview_population}></div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </Layout>
+//     );
+// }
 
-export default Country;
+// export default Country;
 
-export const getServerSideProps = async ({params}) => {
+// export const getServerSideProps = async ({params}) => {
 
-    const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${params.id}`)
+//     const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${params.id}`)
 
-    return {
-        props: {
-            country,
-        },
-    }
-}
+//     return {
+//         props: {
+//             country,
+//         },
+//     }
+// }
